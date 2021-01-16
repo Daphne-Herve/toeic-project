abfd674e06680ce7101b30ea516ff5bc0e7dafb5
Daphne-Herve <62837157+Daphne-Herve@users.noreply.github.com>
2021-01-16 15:54:14 +0100

    Initial commit

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+// src/Controller/WelcomeController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class WelcomeController extends AbstractController
+{
+    /**
+     * @Route("/hello")
+     */
+    public function hello()
+    {
+        return $this->render('welcome/bonjour.html.twig', []);
+    }
+}
